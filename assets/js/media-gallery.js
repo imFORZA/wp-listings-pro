f50064e522f844a5d12d53d86f48d087f62278c4
bradleymoore111 <bradleym007@gmail.com>
2017-03-29 10:31:39 -0700

    adjusted error message on adding wrong filetype

---
+++ assets/js/media-gallery.js
@@ -0,0 +1,322 @@
+var product_gallery_frame;
+var $image_gallery_ids  = jQuery( '#listing_image_gallery' );
+var $product_images     = jQuery( '#listing_images_container' ).find( 'ul.listing_images' );
+jQuery('.add_listing_images').on('click', 'a', function(event){
+  var $el = jQuery( this );
+
+  event.preventDefault();
+
+  // If the media frame already exists, reopen it.
+  if ( product_gallery_frame ) {
+      product_gallery_frame.open();
+      return;
+  }
+
+  // Create the media frame.
+  product_gallery_frame = wp.media.frames.product_gallery = wp.media({
+      // Set the title of the modal.
+      title: $el.data( 'choose' ),
+      button: {
+          text: $el.data( 'update' )
+      },
+      states: [
+          new wp.media.controller.Library({
+              title: $el.data( 'choose' ),
+              filterable: 'all',
+              multiple: true
+          })
+      ]
+  });
+  // When an image is selected, run a callback.
+  product_gallery_frame.on( 'select', function() {
+      console.log("selected");
+      var selection = product_gallery_frame.state().get( 'selection' );
+      var attachment_ids = $image_gallery_ids.val();
+
+      selection.map( function( attachment ) {
+          attachment = attachment.toJSON();
+
+          if ( attachment.id ) {
+							var el = attachment.url.split(".")[attachment.url.split(".").length-1];
+							console.log(el);
+							if(el != "png" && el != "jpg" && el != "jpeg" && el != "gif" && el != "svg"){
+								// Potential TODO: replace alert with a popup, and reopen the medie selector tool rather than close it
+								alert("Only files ending with a .png, .jpg, .jpeg, .gif, or .svg are accepted in this field.");
+								return;
+							}
+              attachment_ids   = attachment_ids ? attachment_ids + ',' + attachment.id : attachment.id;
+							console.log(attachment.url);
+              var attachment_image = attachment.sizes && attachment.sizes.thumbnail ? attachment.sizes.thumbnail.url : attachment.url;
+
+              $product_images.append( '<li class="image" data-attachment_id="' + attachment.id + '"><img src="' + attachment_image + '" /><ul class="actions"><li><a href="#" class="delete" title="' + $el.data('delete') + '">' + $el.data('text') + '</a></li></ul></li>' );
+          }
+      });
+
+      $image_gallery_ids.val( attachment_ids );
+  });
+
+  // Finally, open the modal.
+  product_gallery_frame.open();
+});
+
+jQuery( '#listing_images_container' ).find( 'ul.listing_images' ).sortable({
+	items: 'li.image',
+	cursor: 'move',
+	scrollSensitivity: 40,
+	forcePlaceholderSize: true,
+	forceHelperSize: false,
+	helper: 'clone',
+	opacity: 0.65,
+	placeholder: 'wc-metabox-sortable-placeholder',
+	start: function( event, ui ) {
+		ui.item.css( 'background-color', '#f6f6f6 ' );
+	},
+	stop: function( event, ui ) {
+		ui.item.removeAttr( 'style' );
+	},
+	update: function() {
+		var attachment_ids = '';
+
+		jQuery( '#listing_images_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
+			var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
+			attachment_ids = attachment_ids + attachment_id + ',';
+		});
+
+		$image_gallery_ids.val( attachment_ids );
+	}
+});
+
+// Remove images.
+jQuery( '#listing_images_container' ).on( 'click', 'a.delete', function() {
+	jQuery( this ).closest( 'li.image' ).remove();
+
+	var attachment_ids = '';
+
+	jQuery( '#listing_images_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
+		var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
+		attachment_ids = attachment_ids + attachment_id + ',';
+	});
+
+	$image_gallery_ids.val( attachment_ids );
+
+	// Remove any lingering tooltips.
+	jQuery( '#tiptip_holder' ).removeAttr( 'style' );
+	jQuery( '#tiptip_arrow' ).removeAttr( 'style' );
+
+	return false;
+});
+
+// var $video_gallery_ids  = jQuery( '#listing_video_gallery' );
+// var $product_videos     = jQuery( '#listing_videos_container' ).find( 'ul.listing_videos' );
+// jQuery('.add_listing_videos').on('click', 'a', function(event){
+//   var $el = jQuery( this );
+//
+//   event.preventDefault();
+//
+//   // If the media frame already exists, reopen it.
+//   if ( product_gallery_frame ) {
+//       product_gallery_frame.open();
+//       return;
+//   }
+//
+//   // Create the media frame.
+//   product_gallery_frame = wp.media.frames.product_gallery = wp.media({
+//       // Set the title of the modal.
+//       title: $el.data( 'choose' ),
+//       button: {
+//           text: $el.data( 'update' )
+//       },
+//       states: [
+//           new wp.media.controller.Library({
+//               title: $el.data( 'choose' ),
+//               filterable: 'all',
+//               multiple: true
+//           })
+//       ]
+//   });
+//   // When an video is selected, run a callback.
+//   product_gallery_frame.on( 'select', function() {
+//       console.log("selected");
+//       var selection = product_gallery_frame.state().get( 'selection' );
+//       var attachment_ids = $video_gallery_ids.val();
+//
+//       selection.map( function( attachment ) {
+//           attachment = attachment.toJSON();
+//
+//           if ( attachment.id ) {
+// 							var el = attachment.url.split(".")[attachment.url.split(".").length-1].toLowerCase();
+// 							console.log(el);
+// 							if(el != "mp4" && el != "ogg" && el != "flv" && el != "mkv" && el != "3gp" && el != "webm"){
+// 								// Potential TODO: replace alert with a popup, and reopen the medie selector tool rather than close it
+// 								alert("Only files ending with a .mp4, .ogg, .flv, .mkv, .webm, or .3gp are accepted in this field.");
+// 								return;
+// 							}
+//               attachment_ids   = attachment_ids ? attachment_ids + ',' + attachment.id : attachment.id;
+// 							console.log(attachment.url);
+//               var attachment_video = attachment.sizes && attachment.sizes.thumbnail ? attachment.sizes.thumbnail.url : attachment.url;
+//
+//               $product_videos.append( '<li class="image" data-attachment_id="' + attachment.id + '"><img src="' + attachment_video + '" /><ul class="actions"><li><a href="#" class="delete" title="' + $el.data('delete') + '">' + $el.data('text') + '</a></li></ul></li>' );
+//           }
+//       });
+//
+//       $video_gallery_ids.val( attachment_ids );
+//   });
+//
+//   // Finally, open the modal.
+//   product_gallery_frame.open();
+// });
+//
+// jQuery( '#listing_videos_container' ).find( 'ul.listing_videos' ).sortable({
+// 	items: 'li.image',
+// 	cursor: 'move',
+// 	scrollSensitivity: 40,
+// 	forcePlaceholderSize: true,
+// 	forceHelperSize: false,
+// 	helper: 'clone',
+// 	opacity: 0.65,
+// 	placeholder: 'wc-metabox-sortable-placeholder',
+// 	start: function( event, ui ) {
+// 		ui.item.css( 'background-color', '#f6f6f6 ' );
+// 	},
+// 	stop: function( event, ui ) {
+// 		ui.item.removeAttr( 'style' );
+// 	},
+// 	update: function() {
+// 		var attachment_ids = '';
+//
+// 		jQuery( '#listing_videos_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
+// 			var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
+// 			attachment_ids = attachment_ids + attachment_id + ',';
+// 		});
+//
+// 		$video_gallery_ids.val( attachment_ids );
+// 	}
+// });
+//
+// // Remove videos.
+// jQuery( '#listing_videos_container' ).on( 'click', 'a.delete', function() {
+// 	jQuery( this ).closest( 'li.image' ).remove();
+//
+// 	var attachment_ids = '';
+//
+// 	jQuery( '#listing_videos_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
+// 		var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
+// 		attachment_ids = attachment_ids + attachment_id + ',';
+// 	});
+//
+// 	$video_gallery_ids.val( attachment_ids );
+//
+// 	// Remove any lingering tooltips.
+// 	jQuery( '#tiptip_holder' ).removeAttr( 'style' );
+// 	jQuery( '#tiptip_arrow' ).removeAttr( 'style' );
+//
+// 	return false;
+// });
+
+
+var $doc_gallery_ids  = jQuery( '#listing_doc_gallery' );
+var $product_docs     = jQuery( '#listing_docs_container' ).find( 'ul.listing_docs' );
+jQuery('.add_listing_docs').on('click', 'a', function(event){
+  var $el = jQuery( this );
+
+  event.preventDefault();
+
+  // If the media frame already exists, reopen it.
+  if ( product_gallery_frame ) {
+      product_gallery_frame.open();
+      return;
+  }
+
+  // Create the media frame.
+  product_gallery_frame = wp.media.frames.product_gallery = wp.media({
+      // Set the title of the modal.
+      title: $el.data( 'choose' ),
+      button: {
+          text: $el.data( 'update' )
+      },
+      states: [
+          new wp.media.controller.Library({
+              title: $el.data( 'choose' ),
+              filterable: 'all',
+              multiple: true
+          })
+      ]
+  });
+  // When an doc is selected, run a callback.
+  product_gallery_frame.on( 'select', function() {
+      console.log("selected");
+      var selection = product_gallery_frame.state().get( 'selection' );
+      var attachment_ids = $doc_gallery_ids.val();
+
+      selection.map( function( attachment ) {
+          attachment = attachment.toJSON();
+
+          if ( attachment.id ) {
+							var el = attachment.url.split(".")[attachment.url.split(".").length-1].toLowerCase();
+							console.log(el);
+							if(el != "doc" && el != "docx" && el != "xls" && el != "xlsx" && el != "pdf"){
+								// Potential TODO: replace alert with a popup, and reopen the medie selector tool rather than close it
+								alert("Only files ending with a .doc, .docx, .xls, .xlsx, or .pdf are accepted in this field.");
+								return;
+							}
+              attachment_ids   = attachment_ids ? attachment_ids + ',' + attachment.id : attachment.id;
+							console.log(attachment.url);
+              var attachment_doc = attachment.sizes && attachment.sizes.thumbnail ? attachment.sizes.thumbnail.url : attachment.url;
+
+              $product_docs.append( '<li class="image" data-attachment_id="' + attachment.id + '"><img src="' + attachment_doc + '" /><ul class="actions"><li><a href="#" class="delete" title="' + $el.data('delete') + '">' + $el.data('text') + '</a></li></ul></li>' );
+          }
+      });
+
+      $doc_gallery_ids.val( attachment_ids );
+  });
+
+  // Finally, open the modal.
+  product_gallery_frame.open();
+});
+
+jQuery( '#listing_docs_container' ).find( 'ul.listing_docs' ).sortable({
+	items: 'li.image',
+	cursor: 'move',
+	scrollSensitivity: 40,
+	forcePlaceholderSize: true,
+	forceHelperSize: false,
+	helper: 'clone',
+	opacity: 0.65,
+	placeholder: 'wc-metabox-sortable-placeholder',
+	start: function( event, ui ) {
+		ui.item.css( 'background-color', '#f6f6f6 ' );
+	},
+	stop: function( event, ui ) {
+		ui.item.removeAttr( 'style' );
+	},
+	update: function() {
+		var attachment_ids = '';
+
+		jQuery( '#listing_docs_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
+			var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
+			attachment_ids = attachment_ids + attachment_id + ',';
+		});
+
+		$doc_gallery_ids.val( attachment_ids );
+	}
+});
+
+// Remove docs.
+jQuery( '#listing_docs_container' ).on( 'click', 'a.delete', function() {
+	jQuery( this ).closest( 'li.image' ).remove();
+
+	var attachment_ids = '';
+
+	jQuery( '#listing_docs_container' ).find( 'ul li.image' ).css( 'cursor', 'default' ).each( function() {
+		var attachment_id = jQuery( this ).attr( 'data-attachment_id' );
+		attachment_ids = attachment_ids + attachment_id + ',';
+	});
+
+	$doc_gallery_ids.val( attachment_ids );
+
+	// Remove any lingering tooltips.
+	jQuery( '#tiptip_holder' ).removeAttr( 'style' );
+	jQuery( '#tiptip_arrow' ).removeAttr( 'style' );
+
+	return false;
+});
---
+++ includes/class-listing-gallery-metabox.php
@@ -0,0 +1,318 @@
+<?php
+/**
+ * listing Images
+ *
+ * Display the listing images meta box.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+/**
+ * WPLPRO_Meta_Box_Listing_Images Class.
+ */
+class WPLPRO_Meta_Box_Listing_Images {
+
+	/**
+	 * Output the metabox.
+	 *
+	 * @param WP_Post $post
+	 */
+	public static function output( $post = null, $ok = null) {
+		?>
+		<div id="listing_images_container">
+			<ul class="listing_images">
+				<?php
+					if ( metadata_exists( 'post', $post->ID, '_listing_image_gallery' ) ) {
+						$listing_image_gallery = get_post_meta( $post->ID, '_listing_image_gallery', true );
+					} else {
+						// Backwards compat
+						$listing_image_gallery = get_post_meta( $post->ID, '_listing_image_gallery', true );
+					}
+
+					$attachments         = array_filter( explode( ',', $listing_image_gallery ) );
+					$update_meta         = false;
+					$updated_gallery_ids = array();
+
+					if ( ! empty( $attachments ) ) {
+						foreach ( $attachments as $attachment_id ) {
+							$attachment = wp_get_attachment_image( $attachment_id, 'thumbnail' );
+							//error_log("attachment: " . $attachment);
+							// if attachment is empty skip
+							if ( empty( $attachment ) ) {
+								$update_meta = true;
+								continue;
+							}
+
+							echo '<li class="image" data-attachment_id="' . esc_attr( $attachment_id ) . '">
+								' . $attachment . '
+								<ul class="actions">
+									<li><a href="#" class="delete tips" data-tip="' . esc_attr__( 'Delete image', 'wp-listings-pro' ) .
+									'">' . __( 'Delete', 'wp-listings-pro' ) . '</a></li>
+								</ul>
+							</li>';
+
+							// rebuild ids to be saved
+							$updated_gallery_ids[] = $attachment_id;
+						}
+
+						// need to update listing meta to set new gallery ids
+						if ( $update_meta ) {
+							update_post_meta( $post->ID, '_listing_image_gallery', implode( ',', $updated_gallery_ids ) );
+						}
+					}
+				?>
+			</ul>
+
+			<input type="hidden" id="listing_image_gallery" name="listing_image_gallery" value="<?php echo esc_attr( $listing_image_gallery ); ?>" />
+
+		</div>
+		<p class="add_listing_images hide-if-no-js">
+			<a href="#" data-choose="<?php esc_attr_e( 'Add images to listing gallery', 'wp-listings-pro' ); ?>" data-update="<?php esc_attr_e( 'Add to gallery', 'wp-listings-pro' ); ?>" data-delete="<?php esc_attr_e( 'Delete image', 'wp-listings-pro' ); ?>" data-text="<?php esc_attr_e( 'Delete', 'wp-listings-pro' ); ?>"><?php _e( 'Add listing gallery images', 'wp-listings-pro' ); ?></a>
+		</p>
+
+		<!-- This works -->
+		<!-- <script src="/wp-content/plugins/wp-listings-pro/assets/js/media-gallery.js"></script> -->
+		<!-- TODO: Turn it into a registered and then enqueued script with PROPER LINKING -->
+		<?php
+		wp_enqueue_script( 'class-listings', '/wp-content/plugins/wp-listings-pro/assets/js/media-gallery.js', array('jquery'), null, true );
+		// TODO: move enquement of it
+	}
+
+	/**
+	 * Save meta box data.
+	 *
+	 * @param int $post_id
+	 * @param WP_Post $post
+	 */
+	public static function save( $post_id, $post ) {
+
+		$attachment_ids = isset( $_POST['listing_image_gallery'] ) ? array_filter( explode( ',',  $_POST['listing_image_gallery'] ) ) : array();
+
+		update_post_meta( $post_id, '_listing_image_gallery', implode( ',', $attachment_ids ) );
+	}
+}
+
+// class WPLPRO_Meta_Box_Listing_Videos {
+//
+// 	/**
+// 	 * Output the metabox.
+// 	 *
+// 	 * @param WP_Post $post
+// 	 */
+// 	public static function output( $post = null, $ok = null) {
+// 					? >
+//<!--  		<div id="listing_videos_container">
+//
+// 			<ul class="listing_videos"> -->
+// 					< ?php
+// 					error_log(get_post_meta( $post->ID, '_listing_video_gallery', true));
+// 					if ( metadata_exists( 'post', $post->ID, '_listing_video_gallery' ) ) {
+// 						$listing_video_gallery = get_post_meta( $post->ID, '_listing_video_gallery', true );
+// 					} else {
+// 						// Backwards compat
+// 						$listing_video_gallery = get_post_meta( $post->ID, '_listing_video_gallery', true );
+// 					}
+//
+//
+// 					$attachments         = array_filter( explode( ',', $listing_video_gallery ) );
+// 					$update_meta         = false;
+// 					$updated_gallery_ids = array();
+//
+// 					error_log("though not very fast");
+// 					if ( ! empty( $attachments ) ) {
+// 						error_log("eventually");
+// 						foreach ( $attachments as $attachment_id ) {
+// 							error_log("we're getting data: " . $attachment_id);
+// 							$attachment = wp_get_attachment_image( $attachment_id );
+//
+// 							// if attachment is empty skip
+// 							// if ( empty( $attachment ) ) {
+// 							// 	$update_meta = true;
+// 							// 	continue;
+// 							// }
+// 							error_log("or not");
+//
+// 							echo '<li class="image" data-attachment_id="' . esc_attr( $attachment_id ) . '">
+// 								' . get_the_title($attachment_id) . /*$attachment*/'
+// 								<ul class="actions">
+// 									<li><a href="#" class="delete tips" data-tip="' . esc_attr__( 'Delete video', 'wp-listings-pro' ) .
+// 									'">' . __( 'Delete', 'wp-listings-pro' ) . '</a></li>
+// 								</ul>
+// 							</li>';
+//
+// 							// rebuild ids to be saved
+// 							$updated_gallery_ids[] = $attachment_id;
+// 							error_log("here");
+// 						}
+//
+// 						// need to update listing meta to set new gallery ids
+// 						// WAIT DO WE?!
+// 						// if ( $update_meta ) {
+// 						// 	update_post_meta( $post->ID, '_listing_video_gallery', implode( ',', $updated_gallery_ids ) );
+// 						// }
+//
+// 					}
+// 				? >
+// <!-- // 			</ul>
+//
+//  			<input type="hidden" id="listing_video_gallery" name="listing_video_gallery" value="<?php// echo esc_attr( $listing_video_gallery ); ? >" />
+//
+//  		</div>
+//  		<p class="add_listing_videos hide-if-no-js">
+		// <a href="#" data-choose="< ?php //esc_attr_e( 'Add videos to listing gallery', 'wp-listings-pro' ); ? >" data-update="< ?php //esc_attr_e( 'Add to gallery', 'wp-listings-pro' ); ? >" data-delete="< ?php //esc_attr_e( 'Delete video', 'wp-listings-pro' ); ? >" data-text="< ?php //esc_attr_e( 'Delete', 'wp-listings-pro' ); ? >">< ?php //_e( 'Add listing gallery videos', 'wp-listings-pro' ); ? ></a>
+		// </p>
+  // -->
+	// 	<!-- This works -->
+ // 		<!-- <script src="/wp-content/plugins/wp-listings-pro/assets/js/media-gallery.js"></script> -->
+ // 		<!-- TODO: Turn it into a registered and then enqueued script with PROPER LINKING -->
+ // 		< ? 	php
+// 		wp_enqueue_script( 'class-listings', '/wp-content/plugins/wp-listings-pro/assets/js/media-gallery.js', array('jquery'), null, true );
+// 	}
+//
+// 	/**
+// 	 * Save meta box data.
+// 	 *
+// 	 * @param int $post_id
+// 	 * @param WP_Post $post
+// 	 */
+// 	public static function save( $post_id, $post ) {
+//
+// 		error_log($post_id);
+//
+// 		error_log($_POST['listing_video_gallery']);
+//
+// 		$attachment_ids = isset( $_POST['listing_video_gallery'] ) ? array_filter( explode( ',',  $_POST['listing_video_gallery'] ) ) : array();
+//
+// 		update_post_meta( $post_id, '_listing_video_gallery', implode( ',', $attachment_ids ) );
+// 	}
+// }
+//
+//
+
+/**
+ * WPLPRO_Meta_Box_Listing_Docs Class.
+ */
+ class WPLPRO_Meta_Box_Listing_Docs {
+
+ 	/**
+ 	 * Output the metabox.
+ 	 *
+ 	 * @param WP_Post $post
+ 	 */
+ 	public static function output( $post = null, $ok = null) {
+ 					?>
+ 		<div id="listing_docs_container">
+
+ 			<ul class="listing_docs">
+ 					<?php
+ 					if ( metadata_exists( 'post', $post->ID, '_listing_doc_gallery' ) ) {
+ 						$listing_doc_gallery = get_post_meta( $post->ID, '_listing_doc_gallery', true );
+ 					} else {
+ 						// Backwards compat
+ 						$listing_doc_gallery = get_post_meta( $post->ID, '_listing_doc_gallery', true );
+ 					}
+
+
+ 					$attachments         = array_filter( explode( ',', $listing_doc_gallery ) );
+ 					$update_meta         = false;
+ 					$updated_gallery_ids = array();
+
+ 					if ( ! empty( $attachments ) ) {
+ 						foreach ( $attachments as $attachment_id ) {
+							$attachment_url = wp_get_attachment_url( $attachment_id );
+							$attachment_filetype = wp_check_filetype( $attachment_url )['ext']; // alternate is 'type', yeilds ie: "image/jpeg" instead of "jpg"
+							error_log($attachment_filetype);
+
+							if($attachment_filetype == "xls" || $attachment_filetype == "xlsx"){ // spreadsheet
+								$image_thumbnail = site_url("/wp-includes/images/media/spreadsheet.png");
+							}else{ // doc/pdf
+								$image_thumbnail = site_url("/wp-includes/images/media/document.png");
+							}
+
+							$attachment = sprintf('<img width="150" height="150" src="%s" class="attachment-thumbnail size-thumbnail" alt="" srcset="%s 150w, %s 100w" sizes="100vw" />', $image_thumbnail, $image_thumbnail, $image_thumbnail);
+ 							//$attachment = wp_get_attachment_image( $attachment_id );
+							//error_log($attachment);
+ 							// if attachment is empty skip
+ 							// if ( empty( $attachment ) ) {
+ 							// 	$update_meta = true;
+ 							// 	continue;
+ 							// }
+
+ 							echo '<li class="image" data-attachment_id="' . esc_attr( $attachment_id ) . '">
+							' .  $attachment . '
+							<style>
+								.image .filename{
+									-webkit-font-smoothing: subpixel-antialiased;
+									font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
+									font-size: 12px;
+									user-select: none;
+									color: #444;
+									cursor: pointer;
+									display: block;
+									outline: 0;
+									position: absolute;
+							    left: 0;
+							    right: 0;
+							    bottom: 0;
+							    overflow: hidden;
+							    max-height: 100%;
+							    word-wrap: break-word;
+							    text-align: center;
+							    font-weight: 600;
+							    background: rgba(255,255,255,.8);
+							    -webkit-box-shadow: inset 0 0 0 1px rgba(0,0,0,.15);
+							    box-shadow: inset 0 0 0 1px rgba(0,0,0,.15);
+								}
+							</style>
+							<div class="filename"><div>' .
+								get_the_title($attachment_id) . '.' . $attachment_filetype
+							. ' </div></div>
+
+ 								<ul class="actions">
+ 									<li><a href="#" class="delete tips" data-tip="' . esc_attr__( 'Delete doc', 'wp-listings-pro' ) .
+ 									'">' . __( 'Delete', 'wp-listings-pro' ) . '</a></li>
+ 								</ul>
+ 							</li>';
+
+ 							// rebuild ids to be saved
+ 							$updated_gallery_ids[] = $attachment_id;
+ 						}
+
+ 						// need to update listing meta to set new gallery ids
+ 						// WAIT DO WE?!
+ 						// if ( $update_meta ) {
+ 						// 	update_post_meta( $post->ID, '_listing_doc_gallery', implode( ',', $updated_gallery_ids ) );
+ 						// }
+
+ 					}
+ 				?>
+	</ul>
+
+  			<input type="hidden" id="listing_doc_gallery" name="listing_doc_gallery" value="<?php echo esc_attr( $listing_doc_gallery ); ?>" />
+
+  		</div>
+  		<p class="add_listing_docs hide-if-no-js">
+ 		<a href="#" data-choose="<?php esc_attr_e( 'Add docs to listing gallery', 'wp-listings-pro' ); ?>" data-update="<?php esc_attr_e( 'Add to gallery', 'wp-listings-pro' ); ?>" data-delete="<?php esc_attr_e( 'Delete doc', 'wp-listings-pro' ); ?>" data-text="<?php esc_attr_e( 'Delete', 'wp-listings-pro' ); ?>"><?php _e( 'Add listing gallery docs', 'wp-listings-pro' ); ?></a>
+ 		</p>
+ 		<!-- This works -->
+  		<!-- <script src="/wp-content/plugins/wp-listings-pro/assets/js/media-gallery.js"></script> -->
+  		<!-- TODO: Turn it into a registered and then enqueued script with PROPER LINKING -->
+  		<?php
+ 		wp_enqueue_script( 'class-listings', '/wp-content/plugins/wp-listings-pro/assets/js/media-gallery.js', array('jquery'), null, true );
+ 	}
+
+ 	/**
+ 	 * Save meta box data.
+ 	 *
+ 	 * @param int $post_id
+ 	 * @param WP_Post $post
+ 	 */
+ 	public static function save( $post_id, $post ) {
+
+ 		$attachment_ids = isset( $_POST['listing_doc_gallery'] ) ? array_filter( explode( ',',  $_POST['listing_doc_gallery'] ) ) : array();
+
+ 		update_post_meta( $post_id, '_listing_doc_gallery', implode( ',', $attachment_ids ) );
+ 	}
+ }
