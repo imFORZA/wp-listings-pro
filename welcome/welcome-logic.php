34a4b87038bef9c15b9d9aaa3668c6b85c3c7fe2
bradleymoore111 <bradleym007@gmail.com>
2017-04-05 15:18:11 -0700

    Removes link to welcome page from under plugins, fixes #52

--- welcome/welcome-logic.php
+++ welcome/welcome-logic.php
@@ -65,8 +65,12 @@ function wplpro_welcome_page() {
 	);
 
 }
-
 add_action( 'admin_menu', 'wplpro_welcome_page' );
+
+add_action( 'admin_head', 'remove_menu_entry' );
+function remove_menu_entry(){
+    remove_submenu_page( 'plugins.php', 'wplpro_welcome_page' );
+}
 
 /**
  * Welcome page content.
