5a4ab1ae5f69c0b8837ae8cdd0ef2d51c149bc73
bradleymoore111 <bradleym007@gmail.com>
2017-04-05 08:48:56 -0700

    hotfix for width: 99%

--- includes/views/listing-details-metabox.php
+++ includes/views/listing-details-metabox.php
@@ -117,7 +117,7 @@ echo '<div style="width: 90%; float: left">';
 	echo '<div style="width: 90%; float: left;">';
 
 	_e( '<p><label>Enter Video or Virtual Tour Embed Code (<a href="https://wordpress.org/plugins/jetpack/" target="_blank" rel="nofollow">Jetpack</a> offers several <a href="http://jetpack.me/support/shortcode-embeds/" target="_blank" rel="nofollow">video shortcodes</a>.):<br />', 'wp-listings-pro' );
-	printf( __( '<textarea name="wp_listings[_listing_video]" rows="5" cols="18" style="%1$s">%1$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_video', true ) ) );
+	printf( __( '<textarea name="wp_listings[_listing_video]" rows="5" cols="18" style="%s">%s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_video', true ) ) );
 
 	echo '</div><br style="clear: both;" />';
 
@@ -141,7 +141,7 @@ echo '<div style="width: 90%; float: left">';
 		echo '</div><br style="clear: both;" />';
 
 		_e( '<p><label>Or enter Map Embed Code or shortcode from Map plugin (such as <a href="http://jetpack.me/support/shortcode-embeds/" target="_blank" rel="nofollow">Jetpack Shortcodes</a>, <a href="https://wordpress.org/plugins/simple-google-maps-short-code/" target="_blank" rel="nofollow">Simple Google Maps Short Code</a> or <a href="https://wordpress.org/plugins/mappress-google-maps-for-wordpress/" target="_blank" rel="nofollow">MapPress</a>):<br /><em>Recommend size: 660x300 (If possible, use 100% width, or your themes content width)</em><br />', 'wp-listings-pro' );
-		printf( __( '<textarea name="wp_listings[_listing_map]" rows="5" cols="18" style="%1$s">%1$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_map', true ) ) );
+		printf( __( '<textarea name="wp_listings[_listing_map]" rows="5" cols="18" style="%s">%s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_map', true ) ) );
 
 		echo '</div>';
 
@@ -149,6 +149,6 @@ echo '<div style="width: 90%; float: left">';
 		_e( '<h4>Contact Form</h4>', 'wp-listings-pro' );
 
 		_e( '<p><label>If you use a Contact Form plugin, you may enter the Contact Form shortcode here.', 'wp-listings-pro' );
-		printf( __( '<textarea name="wp_listings[_listing_contact_form]" rows="1" cols="18" style="%1$s">%1$s</textarea></label></p>', 'wp-listings-pro' ), htmlentities( get_post_meta( $post->ID, '_listing_contact_form', true ) ) );
+		printf( __( '<textarea name="wp_listings[_listing_contact_form]" rows="1" cols="18" style="%s">%s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%', htmlentities( get_post_meta( $post->ID, '_listing_contact_form', true ) ) );
 
 		echo '</div><br style="clear: both;" />';
