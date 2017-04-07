329413baf8fd6963044067f86d8ee68df06e7fce
bradleymoore111 <bradleym007@gmail.com>
2017-04-07 12:16:48 -0700

    Setup Lead Tools metabox, fixes #61

--- includes/views/listing-details-metabox.php
+++ includes/views/listing-details-metabox.php
@@ -144,11 +144,3 @@ echo '<div style="width: 90%; float: left">';
 		printf( __( '<textarea name="wp_listings[_listing_map]" rows="5" cols="18" style="%s">%s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_map', true ) ) );
 
 		echo '</div>';
-
-		echo '<div style="width: 90%; float: left;">';
-		_e( '<h4>Contact Form</h4>', 'wp-listings-pro' );
-
-		_e( '<p><label>If you use a Contact Form plugin, you may enter the Contact Form shortcode here.', 'wp-listings-pro' );
-		printf( __( '<textarea name="wp_listings[_listing_contact_form]" rows="1" cols="18" style="%s">%s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%', htmlentities( get_post_meta( $post->ID, '_listing_contact_form', true ) ) );
-
-		echo '</div><br style="clear: both;" />';
