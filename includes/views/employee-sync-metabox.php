5e326ac07f380055440bc5b1568a810287a58c6f
bradleymoore111 <bradleym007@gmail.com>
2017-04-28 12:06:26 -0700

    Redid desecription on wp-listings-settings, removed ?'s and added description to individual listing/agent sync metaboxes

--- includes/views/employee-sync-metabox.php
+++ includes/views/employee-sync-metabox.php
@@ -23,7 +23,7 @@ if($global_sync_setting == 'update-none'){
 
 echo "<div class='idx-import-wrap' style='overflow: auto'>";
 echo "<div class='fixer' style='width:99%;overflow: auto'>";
-_e( '<h2>Update Agent</h2>', 'wp-listings-pro' );
+_e( '<p>Override global options for syncing with IDX feed. Occurs twice daily.</p>', 'wp-listings-pro' );
 _e( '<div class="idx-import-option ' . $global_sync_setting .  '"><label><h4>Follow Global Settings</h4> <span class="dashicons ' . ($global_sync_setting == 'update-all' ? 'dashicons-update' : 'dashicons-dismiss' ) . '"></span><input name="wplpro_agents[_listing_sync_update]" id="_listing_sync_update_global" type="radio" value="update-useglobal" class="code" ' . checked( 'update-useglobal', $sync_setting, false ) . ' /> <p>Follow global settings for updates (is currently set to update ' . ($current_setting) . ').</p></label></div>', 'wp-listings-pro' );
 _e( '<div class="idx-import-option update-all"><label><h4>Update All</h4> <span class="dashicons dashicons-update"></span><input name="wplpro_agents[_listing_sync_update]" id="_listing_sync_update_all" type="radio" value="update-all" class="code" ' . checked( 'update-all', $sync_setting, false ) . ' /> <p>Keep everything up to date. <br /></p></label></div>', 'wp-listings-pro' );
 _e( '<div class="idx-import-option update-none"><label><h4>Do Not Update</h4> <span class="dashicons dashicons-dismiss"></span><input name="wplpro_agents[_listing_sync_update]" id="_listing_sync_update_none" type="radio" value="update-none" class="code" ' . checked( 'update-none', $sync_setting, false ) . ' /> <p><strong>Not recommended as displaying inaccurate MLS data may violate your IDX agreement.</strong><br /> Does not update any fields.<br /></p></label></div>', 'wp-listings-pro' );
