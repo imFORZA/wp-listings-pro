980d9c0092956b513f78c89ff315a30c8c44d110
Brandon Hubbard <bkhubbard@gmail.com>
2017-07-20 16:21:54 -0700

    metabox cleanup

--- includes/class-listings.php
+++ includes/class-listings.php
@@ -229,7 +229,7 @@ class WP_Listings {
 		add_meta_box( 'listing_sync_metabox' , __( 'Sync Settings', 'wp-listings-pro' ), array( &$this, 'listing_sync_metabox' ), 'listing', 'normal', 'high' );
 		add_meta_box( 'listing_details_metabox', __( 'Property Details', 'wp-listings-pro' ), array( &$this, 'listing_details_metabox' ), 'listing', 'normal', 'high' );
 		add_meta_box( 'listing_features_metabox', __( 'Additional Details', 'wp-listings-pro' ), array( &$this, 'listing_features_metabox' ), 'listing', 'normal', 'high' );
-		add_meta_box( 'listing_map_metabox', __( 'Map Options', 'wp-listings-pro' ), array( &$this, 'listing_features_metabox' ), 'listing', 'normal', 'high' );
+		add_meta_box( 'listing_map_metabox', __( 'Map Options', 'wp-listings-pro' ), array( &$this, 'listing_map_metabox' ), 'listing', 'normal', 'high' );
 		add_meta_box( 'listing_contact_metabox', __( 'Lead Tools', 'wp-listings-pro' ), array( &$this, 'listing_contact_metabox' ), 'listing', 'normal', 'high' );
 		add_meta_box( 'listing_assignment_metabox', __( 'Agent Assignments', 'wp-listings-pro' ), array( &$this, 'listing_assignments_metabox' ), 'listing', 'normal', 'high' );
 
--- includes/views/listing-map-metabox.php
+++ includes/views/listing-map-metabox.php
@@ -9,26 +9,55 @@
 if ( ! defined( 'ABSPATH' ) ) { exit;
 }
 
-echo '<div style="width: 90%; float: left;">';
+echo '<div>';
 
-	echo '<div style="width: 45%; float: left">';
-		_e( '<h4>Map Options</h4>', 'wp-listings-pro' );
 
-if ( get_post_meta( $post->ID, '_listing_automap', 1 ) === false ) {
-	update_post_meta( $post->ID, '_listing_automap', 'y' );
-}
-		printf( __( '<p><label>Automatically insert map based on latitude/longitude? <strong>Will be overridden if a shortode is entered below.</strong><br /> <input type="radio" name="wp_listings[_listing_automap]" value="y" %1$s>Yes</input> <input type="radio" name="wp_listings[_listing_automap]" value="n" %2$s>No</input></label></p>' ),
-			checked( get_post_meta( $post->ID, '_listing_automap', true ), 'y', 0 ),
-		checked( get_post_meta( $post->ID, '_listing_automap', true ), 'n', 0 ) );
-		echo '</div>';
-		echo '<div style="clear: both; width: 45%; float: left;">';
-		printf( __( '<p><label>Latitude: <br /><input type="text" name="wp_listings[_listing_latitude]" value="%s" /></label></p>', 'wp-listings-pro' ), get_post_meta( $post->ID, '_listing_latitude', true ) );
-		echo '</div>';
-		echo '<div style="width: 45%; float: right;">';
-		printf( __( '<p><label>Longitude: <br /><input type="text" name="wp_listings[_listing_longitude]" value="%s" /></label></p>', 'wp-listings-pro' ), get_post_meta( $post->ID, '_listing_longitude', true ) );
-		echo '</div><br style="clear: both;" />';
 
-		_e( '<p><label>Or enter Map Embed Code or shortcode from Map plugin (such as <a href="http://jetpack.me/support/shortcode-embeds/" target="_blank" rel="nofollow">Jetpack Shortcodes</a>, <a href="https://wordpress.org/plugins/simple-google-maps-short-code/" target="_blank" rel="nofollow">Simple Google Maps Short Code</a> or <a href="https://wordpress.org/plugins/mappress-google-maps-for-wordpress/" target="_blank" rel="nofollow">MapPress</a>):<br /><em>Recommend size: 660x300 (If possible, use 100% width, or your themes content width)</em><br />', 'wp-listings-pro' );
-		printf( __( '<textarea name="wp_listings[_listing_map]" rows="5" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_map', true ) ) );
+		echo '<h4>' . __( 'Map Details', 'wp-listings-pro' ) . '</h4>';
 
-		echo '</div>';
+		if ( get_post_meta( $post->ID, '_listing_automap', 1 ) === false ) {
+			update_post_meta( $post->ID, '_listing_automap', 'y' );
+		}
+
+		echo '<p>';
+
+		echo '<label>' . __( 'Automatically insert map based on latitude/longitude?', 'wp-listings-pro' ) . '</label>';
+
+		echo '</p>';
+
+		echo __( 'Will be overridden if a shortode is entered below.', '');
+
+		echo '<input type="radio" name="wp_listings[_listing_automap]" value="y" '. checked( get_post_meta( $post->ID, '_listing_automap', true ), 'y', 0 ) .'>Yes</input>';
+
+		echo '<input type="radio" name="wp_listings[_listing_automap]" value="n" '. checked( get_post_meta( $post->ID, '_listing_automap', true ), 'n', 0 ) ) .'>No</input>';
+
+
+		echo '<p>';
+		echo '<label>' . __('Latitude','') . '</label>';
+		echo '<input type="text" name="wp_listings[_listing_latitude]" placeholder="" value="'. get_post_meta( $post->ID, '_listing_latitude', true ) .'" />';
+
+		echo '</p>';
+
+		echo '<p>';
+		echo '<label>' . __('Longitudee','') . '</label>';
+		echo '<input type="text" name="wp_listings[_listing_longitude]" placeholder=" value="' . get_post_meta( $post->ID, '_listing_longitude', true ) . '" />';
+
+		echo '</p>';
+
+		__( 'Enter a Map Embed Code or shortcode from any map plugin', 'wp-listings-pro' );
+
+		__( 'Recommend size: 660x300 (If possible, use 100% width, or your themes content width)', 'wp-listings-pro' );
+
+		echo '<textarea name="wp_listings[_listing_map]" rows="5" cols="18">'. htmlentities( get_post_meta( $post->ID, '_listing_map', true ) )  .'</textarea>';
+
+		echo '<ul>';
+		echo '<li><a href="https://jetpack.me/support/shortcode-embeds/" target="_blank" rel="nofollow">Jetpack Shortcodes</a></li>';
+		echo '<li><a href="https://wordpress.org/plugins/simple-google-maps-short-code/" target="_blank" rel="nofollow">Simple Google Maps Short Code</a></li>';
+		echo '<li><a href="https://wordpress.org/plugins/mappress-google-maps-for-wordpress/" target="_blank" rel="nofollow">MapPress</a></li>';
+		echo '</ul>';
+
+
+
+
+
+echo '</div>';
