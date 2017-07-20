020e059485419378fc9100ece355506e65d7b67b
Brandon Hubbard <bkhubbard@gmail.com>
2017-07-20 16:49:36 -0700

    better additional details metabox

--- includes/views/listing-features-metabox.php
+++ includes/views/listing-features-metabox.php
@@ -9,53 +9,45 @@
 if ( ! defined( 'ABSPATH' ) ) { exit;
 }
 
-// Featured on.
+// Post.
 global $post;
 
-echo '<div style="width: 90%; float: left;">';
 
-	printf( __( '<p><label>Featured on (allows shortcodes):<br /><textarea name="wp_listings[_listing_featured_on]" rows="3" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_featured_on', true ) ) );
+	// Featured On.
+	echo '<label><strong>' . __('Featured on:','wp-listings-pro') . '</strong></label>';
+	echo '<textarea name="wp_listings[_listing_featured_on]" id="" class="" rows="3" cols="18" style="width:100%;">' . htmlentities( get_post_meta( $post->ID, '_listing_featured_on', true ) ) . '</textarea>';
+	echo '<p class="description">'. __( 'This field allows you to display any details about where this listings has been featured, such as media sites.', 'wp-listings-pro') .'</p>';
 
-echo '</div><br style="clear: both;" />';
+	// Home Summary.
+	echo '<label><strong>' . __('Home Summary:','wp-listings-pro') . '</strong></label>';
+	echo '<textarea name="wp_listings[_listing_home_sum]" id="" class="" rows="3" cols="18" style="width:100%;">' .  htmlentities( get_post_meta( $post->ID, '_listing_home_sum', true ) ) . '</textarea>';
+	echo '<p class="description">'. __( 'This field allows you to display any quick home summary details.', 'wp-listings-pro') .'</p>';
 
-// Home Summary.
-echo '<div style="width: 90%; float: left;">';
+	// Kitchen Summary.
+	echo '<label><strong>' . __('Kitchen Summary:','wp-listings-pro') . '</strong></label>';
+	echo '<textarea name="wp_listings[_listing_kitchen_sum]" id="" class="" rows="3" cols="18" style="width:100%;">' . htmlentities( get_post_meta( $post->ID, '_listing_kitchen_sum', true ) ) . '</textarea>';
+	echo '<p class="description">'. __( 'This field allows you to display any quick kitchen summary details.', 'wp-listings-pro') .'</p>';
 
-	printf( __( '<p><label>Home Summary (allows shortcodes):<br /><textarea name="wp_listings[_listing_home_sum]" rows="3" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_home_sum', true ) ) );
 
-echo '</div><br style="clear: both;" />';
+	// Living Room.
+	echo '<label><strong>' . __('Living Room:','wp-listings-pro') . '</strong></label>';
+	echo '<textarea name="wp_listings[_listing_living_room]" id="" class="" rows="3" cols="18" style="width:100%;">' . htmlentities( get_post_meta( $post->ID, '_listing_living_room', true ) ) . '</textarea>';
+	echo '<p class="description">'. __( 'This field allows you to display any quick living room summary details.', 'wp-listings-pro') .'</p>';
 
-// Kitchen Summary.
-echo '<div style="width: 90%; float: left;">';
 
-	printf( __( '<p><label>Kitchen Summary (allows shortcodes):<br /><textarea name="wp_listings[_listing_kitchen_sum]" rows="3" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_kitchen_sum', true ) ) );
+	// Master Suite.
+	echo '<label><strong>' . __('Master Suite:','wp-listings-pro') . '</strong></label>';
+	echo '<textarea name="wp_listings[_listing_master_suite]" id="" class="" rows="3" cols="18" style="width:100%;">' . htmlentities( get_post_meta( $post->ID, '_listing_master_suite', true ) ) . '</textarea>';
+	echo '<p class="description">'. __( 'This field allows you to display any quick master suite summary details.', 'wp-listings-pro') .'</p>';
 
-echo '</div><br style="clear: both;" />';
+	// School and Neighborhood Info.
+	echo '<label><strong>' . __('School and Neighborhood:','wp-listings-pro') . '</strong></label>';
+	echo '<textarea name="wp_listings[_listing_school_neighborhood]" id="" class="" rows="3" cols="18" style="width:100%;">' . htmlentities( get_post_meta( $post->ID, '_listing_school_neighborhood', true ) ) . '</textarea>';
+	echo '<p class="description">'. __( 'This field allows you to display any quick school and neighborhood summary details.', 'wp-listings-pro') .'</p>';
 
-// Living Room.
-echo '<div style="width: 90%; float: left;">';
 
-	printf( __( '<p><label>Living Room (allows shortcodes):<br /><textarea name="wp_listings[_listing_living_room]" rows="3" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_living_room', true ) ) );
+	// Disclaimer.
+	echo '<label><strong>' . __('Disclaimer:','wp-listings-pro') . '</strong></label>';
+	echo '<textarea name="wp_listings[_listing_disclaimer]" id="" class="" rows="3" cols="18" style="width:100%;">' . htmlentities( get_post_meta( $post->ID, '_listing_disclaimer', true ) ) . '</textarea>';
+	echo '<p class="description">'. __( 'This field allows you to display a disclaimer for the listing.', 'wp-listings-pro') .'</p>';
 
-echo '</div><br style="clear: both;" />';
-
-// Master Suite.
-echo '<div style="width: 90%; float: left;">';
-
-	printf( __( '<p><label>Master Suite (allows shortcodes):<br /><textarea name="wp_listings[_listing_master_suite]" rows="3" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_master_suite', true ) ) );
-
-echo '</div><br style="clear: both;" />';
-
-// School and Neighborhood Info.
-echo '<div style="width: 90%; float: left;">';
-
-	printf( __( '<p><label>School and Neighborhood Info (allows shortcodes):<br /><textarea name="wp_listings[_listing_school_neighborhood]" rows="5" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_school_neighborhood', true ) ) );
-
-echo '</div><br style="clear: both;" />';
-
-// Disclaimer.
-echo '<div style="width: 90%; float: left;">';
-
-	printf( __( '<p><label>Disclaimer:<br /><textarea name="wp_listings[_listing_disclaimer]" rows="3" cols="18" style="%1$s">%2$s</textarea></label></p>', 'wp-listings-pro' ), 'width: 99%;', htmlentities( get_post_meta( $post->ID, '_listing_disclaimer', true ) ) );
-
-echo '</div><br style="clear: both;" />';
