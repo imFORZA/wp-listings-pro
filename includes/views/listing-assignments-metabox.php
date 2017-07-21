e75ddb00eded573e649ec2ca368625cf4bc7565c
Brandon Hubbard <bkhubbard@gmail.com>
2017-07-20 17:15:09 -0700

    cleanup

--- includes/views/listing-assignments-metabox.php
+++ includes/views/listing-assignments-metabox.php
@@ -10,17 +10,20 @@ if ( ! defined( 'ABSPATH' ) ) { exit;
 
 // * Agent Assignments.
 global $post;
-echo '<div>';
-_e( '<h4>Agent Assignments</h4><p>', 'wp-listings-pro' );
+
+
+echo '<h4>'. __( 'Agent Assignments', 'wp-listings-pro' ) .'</h4>';
+
 $stuff = get_posts(array(
 	'post_type'       => 'employee',
 	'posts_per_page'  => 500,
 ));
+
 if ( count( $stuff ) === 0 ) {
 	echo __( 'No agents found.', 'wp-listings-pro' );
 } else {
 	$agent_assignments = explode( ',', get_post_meta( $post->ID, '_employee_responsibility' , true ) );
-	// $agent_assignments = [5, 15]; // Now just need to ba able to get this from post meta
+
 	foreach ( $stuff as $agent ) {
 		printf( '<input type="checkbox" name="wp_listings[_employee_responsibility_' . $agent->ID . ']" %s />',checked( in_array( $agent->ID, $agent_assignments ), 1, 0 ) );
 
@@ -28,5 +31,3 @@ if ( count( $stuff ) === 0 ) {
 		echo '<br>';
 	}
 }
-
-echo '</p></div>';
