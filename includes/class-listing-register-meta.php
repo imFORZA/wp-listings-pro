7ae24d27fbdc92a72c9fcd8909d539d94f9239d8
Brandon Hubbard <bkhubbard@gmail.com>
2017-08-27 22:32:49 -0700

    register meta

---
+++ includes/class-listing-register-meta.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+register_meta( 'listing', '_listing_sync_update', array(
+		'sanitize_callback' => 'sanitize_listing_sync_update',
+		'auth_callback' => 'authorize_listing_sync_update',
+		'type' => 'string',
+		'description' => __('My registered meta key', 'wp-listings-pro'),
+		'single' => true,
+		'show_in_rest' => true,
+	)
+);
+
+
+function authorize_listing_sync_update() {
+
+}
+
+function sanitize_listing_sync_update() {
+
+}
